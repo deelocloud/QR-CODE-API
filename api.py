7924507aa72446a2a6ae4358b3098578470faac8
Om Mishra <96434205+Om-Mishra7@users.noreply.github.com>
2022-08-30 10:33:03 +0530

    Create api.py

---
+++ api.py
@@ -0,0 +1,24 @@
+# Made by ProjectRexa
+# Check Documentaion - https://projectrexa.ml/api-documentation/
+
+from flask import Flask,request,send_file
+from flask_qrcode import QRcode
+import re
+
+app = Flask(__name__)
+
+qrcode = QRcode(app)
+
+@app.route('/')
+def home():
+    query = request.query_string
+    query = query.decode('utf-8')
+    query = query.replace("%20"," ")
+    query = str(re.sub(r"[^a-zA-Zé,.-0123456789';:_()]", ' ', query))
+    return send_file(qrcode(query, mode="raw"), mimetype="image/png")
+
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0")
